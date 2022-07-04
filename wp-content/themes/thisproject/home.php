590c2013bd66a63ba502d225ba43b4d68fdb479c
adi <95744589+adiwork@users.noreply.github.com>
2022-07-04 16:20:54 +0530

    Content: Section: About Us: Move into a dedicated file

--- wp-content/themes/thisproject/home.php
+++ wp-content/themes/thisproject/home.php
@@ -253,54 +253,10 @@ require_once __DIR__ . '/partials/header.php';
 </div>
 <!-- END: Institutes Section -->
 
-<!-- About Us Section -->
-<section class="about-us-section space-200-top-bottom" id="about-us-section" data-section-title="About Us Section" data-section-slug="about-us-section">
-	<div class="container">
-		<div class="columns small-offset-1 small-10 medium-offset-0 medium-5 large-4 space-150-bottom">
-			<div class="logo h1 strong text-purple-2 visuallyhidden">bursar</div>
-			<img class="logo large" src="<?= THEME_URI ?>/media/logos/logo-bursar-large-color.svg<?= $ver ?>" alt="bursar logo with tagline">
-		</div>
+<?php require_once __DIR__ . '/sections/about-us.php'; ?>
 
-		<div class="row">
-			<div class="columns small-offset-1 small-10 medium-offset-0 medium-12 large-8">
-				<div class="h3 strong text-dark space-25-bottom">About us.</div>
-				<div class="columns small-12 medium-12 large-10 space-50-top space-150-bottom">
-					<div class="h6 text-dark">A group of experienced, fun to work with, customer-focused individuals based out of Bangalore with significant experience in the education sector, complemented by long-standing associations with top-ranked educational institutions. We are passionate about changing student lending and simplifying the borrowing process.</div>
-				</div>
-			</div>
 
-			<div class="columns small-offset-1 small-10 medium-offset-0 medium-8 large-offset-1 large-3">
-				<div class="row space-50-top">
-					<div class=" columns small-12 medium-8 large-12 space-100-bottom">
-						<div class="h-social h5 strong text-dark space-50-bottom"><span>Connect with us</span></div>
-						<div class="social-icons">
-							<a href="https://twitter.com/TechBursar" target="_blank"><img class="block button fill-indigo-2" src="<?= THEME_URI ?>/media/icons/icon-social-twitter-white.svg<?= $ver ?>" alt="Twitter"></a>
-							<a href="https://www.linkedin.com/company/bursarpayments/" target="_blank"><img class="block button fill-indigo-2" src="<?= THEME_URI ?>/media/icons/icon-social-linkedin-white.svg<?= $ver ?>" alt="LinkedIn"></a>
-						</div>
-						<a class="columns small-8 large-9 space-50-bottom" href="mailto:hello@bursar.in">
-							<span class="block button strong fill-indigo-2 text-white text-left" style="padding-left: 0;"><img class="button-icon" src="<?= THEME_URI ?>/media/icons/icon-email-white.svg<?= $ver ?>">hello@bursar.in</span>
-						</a>
-						<a class="columns small-8 large-9 space-50-bottom" href="tel:+91-99455-01800">
-							<span class="block button strong fill-indigo-2 text-white text-left" style="padding-left: 0;"><img class="button-icon" src="<?= THEME_URI ?>/media/icons/icon-phone-white.svg<?= $ver ?>">+91-99455-01800</span>
-						</a>
-					</div>
-					<nav class="columns small-12 medium-4 large-12">
-						<div class="h-nav h5 strong text-dark space-25-bottom"><span>Links</span></div>
-						<ul class="p strong text-indigo-2">
-							<li><a class="space-25-top-bottom" href="/">Home</a></li>
-							<li><a class="space-25-top-bottom" href="/#study-now-pay-later-section">SNPL</a></li>
-							<li><a class="space-25-top-bottom" href="/#why-us-section">Why Us</a></li>
-							<li><a class="space-25-top-bottom" href="/#lenders-section">Lenders</a></li>
-							<li><a class="space-25-top-bottom" href="/#partner-with-us-section">Partner with Us</a></li>
-							<li><a class="space-25-top-bottom" href="/#institutes-section">Institutes</a></li>
-							<li><a class="space-25-top-bottom" href="/#about-us-section">Team</a></li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- END: About Us Section -->
+
+
 
 <?php require_once __DIR__ . '/partials/footer.php'; ?>
